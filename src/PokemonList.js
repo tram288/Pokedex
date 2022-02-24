18d95414de02d8a30404fe3d3ba69342188f01c1
Matthew Chuong <mstchuong@yahoo.com>
2022-02-23 22:30:49 -0500

    Remove PokemonCard from list
    update css

--- src/PokemonList.js
+++ src/PokemonList.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import axios from 'axios';
-import PokemonCard from './PokemonCard';
 import PokemonCardV2 from './PokemonCardV2';
 import _ from 'lodash';
 
@@ -8,16 +7,11 @@ export default class PokemonList extends React.Component {
   constructor(prop) {
     super(prop);
     this.state = {
-      pokemon: {},
       pokemons: [],
     };
   }
 
   componentDidMount = async () => {
-    await axios.get(`https://pokeapi.co/api/v2/pokemon/bulbasaur`).then((res) => {
-      const pokemon = res.data;
-      this.setState({ pokemon: pokemon });
-    });
     await axios.get(`https://pokeapi.co/api/v2/pokemon`).then((res) => {
       const pokemons = res.data.results;
       this.setState({ pokemons: pokemons });
@@ -26,14 +20,12 @@ export default class PokemonList extends React.Component {
 
   render() {
     const { pokemons } = this.state;
-    const pokemon = _.get(this.state, 'pokemon');
 
     return (
       <>
-        <PokemonCard key={_.get(pokemon, 'name')} pokemon={pokemon} />
-        {/* <div className='PokemonList'>
+        <div className='PokemonList'>
           {_.map(pokemons, pokemon => <PokemonCardV2 key={pokemon.name} pokemon={pokemon} />)}
-        </div> */}
+        </div>
       </>
     );
   }
